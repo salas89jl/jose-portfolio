2c6d70f9b1ba16639c7b829ad602792fce5465a7
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-06-12 05:34:56 -0700

    added flowbite components and project data

---
+++ app/comps/Navbar.js
@@ -0,0 +1,51 @@
+'use client';
+
+import { Button, Navbar } from 'flowbite-react';
+
+export default function NavBar() {
+  return (
+    <Navbar
+      fluid
+      rounded
+    >
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img
+          alt="Flowbite React Logo"
+          className="mr-3 h-6 sm:h-9"
+          src="/favicon.svg"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Flowbite React
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button href='https://acrobat.adobe.com/link/review?uri=urn%3Aaaid%3Ascds%3AUS%3Af27f23b8-6112-43a2-8d10-55f7acc58f74'>
+          Resumé
+        </Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link
+          active
+          href="#"
+        >
+          <p>
+            Home
+          </p>
+        </Navbar.Link>
+        <Navbar.Link href="#">
+          About
+        </Navbar.Link>
+        <Navbar.Link href="#">
+          Services
+        </Navbar.Link>
+        <Navbar.Link href="#">
+          Pricing
+        </Navbar.Link>
+        <Navbar.Link href="#">
+          Contact
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
