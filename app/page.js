5b708c8305ead40ad0991e96a61dc6afc0292f30
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-06-16 13:54:58 -0700

    formatted margins and padding

--- app/page.js
+++ app/page.js
@@ -17,33 +17,33 @@ export default function Home () {
     <main className='bg-gradient-to-t from-cyan-500 to-slate-800  text-white'>
       <NavBar className='bg-slate-800' />
       <section className='min-h-auto flex flex-wrap justify-evenly motion-safe' >
-        <div className='text-center p-10 rounded-xl mt-10 mx-10 flex-1 motion-safe:'>
+        <div className='text-center p-10 rounded-xl mt-20 mx-10 flex-1 motion-safe:'>
            <h1 className="  text-5xl py-1 bg-gradient-to-r from-teal-400 via-cyan-500 to-indigo-400 inline-block text-transparent bg-clip-text font-bold">Jose Salas</h1>
            <h2 className='text-5xl py-1 text-teal-500 font-medium'></h2>
            <div className='relative mx-auto '>
-            <Image src={avatar} className='mx-auto shadow-lg opacity-90 rounded-md w-80 h-70 mt-10 ' />
+            <Image src={avatar} className='mx-auto shadow-2xl opacity-90 rounded-md w-80 h-70 mt-10 ' />
             </div>
             <div className='flex flex-wrap justify-center pt-10'>
-            <div className="word p-10 text-center"><AniText /></div>
-          </div>
+              <div className="absolute word p-10 text-center"><AniText /></div>
+            </div>
         </div>
-        <div id='aboutId' className=' slide-in-right text-center p-10 rounded-xl mt-40 flex-1 '>
+        <div id='aboutId' className=' slide-in-right text-center p-10 rounded-xl mt-32 flex-1 mx-auto '>
           <h3 className='   text-3xl h-14 p-3 bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-medium flex justify-center rounded-2xl '>About Me</h3>
-          <p className='text-xl p-5 bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 tracking-wide text-white rounded-lg shadow-xl justfiy-center m-5'>
-          I am excited to announce that I am embarking on a new journey as a full stack developer after retiring as a Boatswains Mate Chief Petty Officer. I am eager to utilize my skills in problem-solving, attention to detail, and teamwork in this dynamic field. With a passion for technology and a desire to continuously learn, I am confident that I can make a meaningful impact as a full stack developer. I look forward to connecting with like-minded individuals and companies who share the same passion for innovation and growth.
+          <p className='text-lg p-5 bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 tracking-wide text-white rounded-lg shadow-xl justfiy-center m-5'>
+          I am excited to announce that I am embarking on a new journey as a full-stack developer after retiring as a Boatswains Mate Chief Petty Officer. I am eager to utilize my skills in problem-solving, attention to detail, and teamwork in this dynamic field. With a passion for technology and a desire to continuously learn, I am confident that I can make a meaningful impact as a full-stack developer. I look forward to connecting with like-minded individuals and companies with the same passion for innovation and growth.
           </p>
         </div>
       </section>
-      <section id='workId'className=' min-h-auto flex flex-wrap justify-center md:flex-row'>
-        <div className='bg-gradient-to-bl   from-teal-400 via-cyan-500 to-cyan-600 text-center shadow-xl p-10 rounded-xl m-10 flex-1'>
+      <section id='workId' className=' min-h-auto flex flex-wrap justify-center '>
+        <div className='bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-10 mx-10 flex-1 motion-safe:'>
           <Image src='/code.png' width={100} height={100} className='mx-auto' />
-          <div className=' p-10 m-auto rounded-lg flex flex-wrap justify-center'>
+          <div className=' mt-6 p-10 m-auto rounded-lg flex flex-wrap justify-center bg-white shadow-xl'>
 
            <Icons />
 
           </div>
         </div>
-        <div className=' bg-gradient-to-br f from-teal-400 via-cyan-500 to-cyan-600 text-center shadow-xl p-10 rounded-xl  m-10 flex-1'>
+        <div className=' bg-gradient-to-br f from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-10 mx-10 flex-1 motion-safe:'>
           <Image src={design} width={100} height={100} className='mx-auto' />
           <h3 className='text-lg font-medium pt-8 pb-2 text-white'>Beautiful Designs</h3>
           <p className='py-2 text-white'>
@@ -54,10 +54,10 @@ export default function Home () {
         </div>
       </section>
       <section id='projectsId'>
-        <div className='bg-gradient-to-b from-teal-400 via-cyan-500 to-cyan-600 p-5 m-10 rounded-xl shadow-xl text-center'>
-          <h3 className='text-lg font-medium pt-8 pb-2 text-white py-1'>Projects</h3>
+        <div className='bg-gradient-to-tl from-teal-400 via-cyan-500 to-cyan-600 p-5 m-10 rounded-xl shadow-xl text-center'>
+          <h3 className='text-xl font-medium pt-8 pb-2 text-white py-1'>Projects</h3>
             <p className="text-md py-2 leading-8 text-white dark:text-gray-200">
-              I offer from a wide range of services, including design and programming.
+            I offer a wide range of services, including design and programming.
             </p>
             <div>
               <Projects />
--- app/projects.js
+++ app/projects.js
@@ -4,28 +4,28 @@ const projects = [
   {
     id: 1,
     title: 'Cat Tinder',
-    description: 'This is a application built for cat to find thier purrfect match',
+    description: 'This is an application built for a cat to find their purrfect match.',
     github: 'https://github.com/salas89jl/cat-tinder-frontend-meow-2-meow',
     image: '/m2m.png'
   },
   {
     id: 2,
     title: 'Casserio World Wide',
-    description: 'This is a appartment listing application where users can upload and find there perfect home. ',
+    description: 'This apartment listing application allows users to upload and find their perfect home.',
     github: 'https://github.com/salas89jl/apartment-app-frontend-caserio-world-wide',
     image: '/cww.png'
   },
   {
     id: 3,
     title: 'ARA',
-    description: "ARA is an online platform dedicated solely to the world of anime. Our mission is simple: to provide you with a comprehensive database of anime content so that you can find what you like, not only what's currently popular. ",
+    description: "ARA is an online platform dedicated solely to the world of anime. Our mission is simple: to provide a comprehensive database of anime content to find what you like, not only what's currently popular. ",
     github: 'https://github.com/salas89jl/apartment-app-frontend-caserio-world-wide',
     image: '/ara.png'
   },
   {
     id: 3,
     title: 'Text-Base Murder Mystery',
-    description: 'This game takes place in the terminal. The interactions between the user and the game are text-based. The user will see a story printed to the terminal and then be presented with options that the will determine the next phase of the story. Users will type their selection into the terminal.',
+    description: `This game takes place in the terminal. The interactions between the user and the game are text-based. The user will see a story printed on the terminal and then be presented with options that will determine the story's next phase. Users will type their selection into the terminal.`,
     github: 'https://github.com/salas89jl/text-based-game-murder-mystery',
     image: '/text-base-game.png'
   }
