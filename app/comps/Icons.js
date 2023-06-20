2d6d2e75b2bcefe5a89b33c02c9d03ef537254bc
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-06-20 06:45:05 -0700

    styling to section gradients and padding

--- app/comps/Icons.js
+++ app/comps/Icons.js
@@ -4,7 +4,7 @@ export default function Icons () {
   return (
         <>
             {icons.map((icon) => (
-                <div key={icon.id}>
+                <div key={icon.id} className='bg-gradient-to-bl  from-gray-800 via-gray-700 to-gray-900 shadow-xl shadow-sky-900 rounded-lg m-1 p-1'>
                     <img src={icon.src} className='h-16 p-2' />
                 </div>
             ))}
--- app/page.js
+++ app/page.js
@@ -16,14 +16,14 @@ import TreasureHunt from './comps/TreasureHunt'
 export default function Home () {
   return (
 
-    <main className='bg-gradient-to-t from-cyan-500 to-slate-800  text-white'>
+    <main className='bg-gradient-to-tl from-cyan-600 via-cyan-700 to-slate-800  text-white'>
       <NavBar />
       <section className='min-h-auto flex flex-wrap justify-evenly motion-safe' >
         <div className='text-center p-10 rounded-xl mt-20 mx-10 flex-1 motion-safe:'>
-           <h1 className="  text-5xl py-1 bg-gradient-to-r from-teal-400 via-cyan-500 to-indigo-400 inline-block text-transparent bg-clip-text font-bold">Jose Salas</h1>
+           <h1 className="  text-6xl py-1 bg-gradient-to-r from-teal-400 via-cyan-500 to-indigo-400 inline-block text-transparent bg-clip-text font-bold">Jose Salas</h1>
            <h2 className='text-5xl py-1 text-teal-500 font-medium'></h2>
            <div className='relative mx-auto '>
-            <Image src={avatar} className='mx-auto shadow-xl shadow-slate-800  opacity-90 rounded-md w-80 h-70 mt-10 ' />
+            <Image src={avatar} className=' mx-auto shadow-xl shadow-slate-800  rounded-md w-80 h-70 mt-10 ' />
             </div>
             <div className='flex flex-wrap justify-center pb-10 '>
               <div className="absolute word p-10 text-center"><AniText /></div>
@@ -32,7 +32,7 @@ export default function Home () {
 
       </section>
       <section>
-      <div id='aboutId' className=' slide-in-right text-center p-10 rounded-xl mt-20 flex-1 mx-auto '>
+      <div id='aboutId' className=' slide-in-right text-center p-10 rounded-xl mt-10 flex-1 mx-auto '>
           <h3 className='   text-3xl  p-3 m-5 bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-medium flex justify-center rounded-xl shadow-xl shadow-slate-800  '>About Me</h3>
           <p className='text-lg p-5 bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 tracking-wide text-white rounded-lg shadow-xl shadow-slate-800 justfiy-center m-5'>
           I am excited to announce that I am embarking on a new journey as a full-stack developer after retiring as a Boatswains Mate Chief Petty Officer. I am eager to utilize my skills in problem-solving, attention to detail, and teamwork in this dynamic field. With a passion for technology and a desire to continuously learn, I am confident that I can make a meaningful impact as a full-stack developer. I look forward to connecting with like-minded individuals and companies with the same passion for innovation and growth.
@@ -40,16 +40,16 @@ export default function Home () {
         </div>
       </section>
       <section id='workId' className=' min-h-auto flex flex-wrap justify-center '>
-        <div className='bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-10 mx-10 flex-1 shadow-xl shadow-sky-900  motion-safe:'>
-          <Image src='/code2.png' width={100} height={100} className='mx-auto shadow-lg shadow-sky-800 rounded-full' />
+        <div className='bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-10 mx-10 flex-1 shadow-xl shadow-sky-950  motion-safe:'>
+          <Image src='/code2.png' width={100} height={100} className='mx-auto shadow-lg shadow-sky-800 rounded-full border-4 border-cyan-500' />
           <h3 className="text-xl font-medium pt-8 pb-2  text-white">Code</h3>
-          <div className=' mt-6 p-10 m-auto rounded-lg flex flex-wrap justify-center bg-gradient-to-bl from-cyan-400 to-cyan-600 shadow-xl shadow-sky-900 '>
+          <div className=' mt-6 p-10 mx-10 rounded-lg flex flex-wrap justify-center bg-gradient-to-bl from-cyan-400 to-cyan-600 shadow-xl shadow-sky-900 '>
            <Icons />
           </div>
         </div>
       </section>
       <section id='techTalk'>
-      <div className="bg-gradient-to-br from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-20 mx-10 flex-1 shadow-xl shadow-sky-900 motion-safe">
+      <div className="bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-20 mx-10 flex-1 shadow-xl shadow-sky-950 motion-safe">
           <Image src={lecture4} width={100} height={100} className="mx-auto  rounded-full shadow-lg shadow-sky-800" />
           <h3 className="text-xl font-medium pt-8 pb-2 text-white">Tech Talks</h3>
           <div className="mt-6 p-4 rounded-lg flex flex-wrap justify-center bg-gradient-to-bl from-cyan-400 to-cyan-600 shadow-xl shadow-sky-900 ">
@@ -62,7 +62,7 @@ export default function Home () {
         </div>
       </section>
       <section id='treasureHunt'>
-        <div className="bg-gradient-to-br from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-20 mx-10 flex-1 shadow-xl shadow-sky-900 motion-safe" >
+        <div className="bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 text-center p-10 rounded-xl mt-20 mx-10 flex-1 shadow-xl shadow-sky-950 motion-safe" >
           <div >
             <TreasureHunt />
           </div>
@@ -70,9 +70,9 @@ export default function Home () {
       </section>
       <section id='projectsId'>
         <div className='
-        bg-gradient-to-br from-teal-400 via-cyan-500 to-cyan-600 m-10 rounded-xl border-4 border-cyan-500
-        p-10 shadow-xl shadow-sky-900  text-center'>
-        <Image src={'/projects2.png'} width={100} height={100} className="mx-auto  rounded-full border-1 border-cyan-500 shadow-lg shadow-black" />
+        bg-gradient-to-bl from-teal-400 via-cyan-500 to-cyan-600 mx-10 mt-20 rounded-xl
+        p-10 shadow-xl shadow-sky-950  text-center'>
+        <Image src={'/projects2.png'} width={100} height={100} className="mx-auto  rounded-full border-1 border-cyan-500 shadow-lg shadow-sky-800" />
           <h3 className='text-xl font-medium pt-8  text-white py-2'>Projects</h3>
             <p className="text-md py-6 leading-8 text-white dark:text-gray-200">
             I offer a wide range of services, including design and programming!
@@ -83,7 +83,7 @@ export default function Home () {
         </div>
 
       </section>
-      <div id='contactId' className='bg-black shadow-lg shadow-sky-900  m-10 p-10 rounded-xl flex-1' >
+      <div id='contactId' className='bg-black shadow-lg shadow-sky-950  m-10 p-10 rounded-xl flex-1' >
           <div className='text-5xl flex justify-center gap-10 py-2'>
             <a href="https://github.com/salas89jl"><AiFillGithub className='text-white'/></a>
             <a href="https://www.linkedin.com/in/joselsalas/"><AiFillLinkedin className='text-white'/></a>
